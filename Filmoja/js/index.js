8c92deb6233f14cb90b8422c0e46ab972e4a9e49
alakbar97 <alakbarsa@code.edu.az>
2019-06-04 23:49:55 +0400

    need to fix image  and width owl carousel

--- Filmoja/js/index.js
+++ Filmoja/js/index.js
@@ -9,3 +9,22 @@ $(function () {
             $(this).toggleClass('open1');
         });
 });
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
